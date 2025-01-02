5c30680a8c864d04e93748700820f1970080551b
Stanislav Efimtsev <evendot@yandex.ru>
2025-01-02 22:03:32 +0300

    Connected to the PostgreSQL DB

--- src/main/java/ru/evendot/toy_shop/Models/Toy.java
+++ src/main/java/ru/evendot/toy_shop/Models/Toy.java
@@ -1,11 +1,16 @@
 package ru.evendot.toy_shop.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "toys")
 public class Toy {
+    @Id
+    private Integer id;
     private String title;
     private Integer article;
     private String description;
